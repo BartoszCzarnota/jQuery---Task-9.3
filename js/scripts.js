26339210b4f3c673dbba06f6f51adf27455c499c
Bartosz CZarnota <bartek.bootcamp@gmail.com>
2017-04-13 18:26:01 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(function() {
+
+	var carouselList = $("#carousel ul");
+			leftarrow = $("#left");
+			rightarrow = $("#right");
+
+	
+
+		leftarrow.on("click", function() {
+			carouselList.animate({'marginLeft': -600}, 1100, function(){
+					$(this).find("li:last").after($(this).find("li:first"));
+					$(this).css({marginLeft:0});
+			});
+		});
+
+		rightarrow.on("click", function() {
+			carouselList.animate({'marginRight': 1200}, 2200, function(){
+					$(this).find("li:last").after($(this).find("li:first"));
+					$(this).css({marginLeft: 0});
+			});
+		});
+
+
+
+		carouselList.each(function(){
+			setInterval(function(){
+				carouselList.animate({'marginLeft': -600}, 1100, function(){
+
+					$(this).find("li:last").after($(this).find("li:first"));
+					$(this).css({marginLeft:0});
+
+				});
+			}, 5000);
+		});
+});
